ef880a3ffe517070ae3f96d7cd04ea3466d2b7fb
vinod kumar <vinukumar831@gmail.com>
2019-02-25 08:50:31 -0800

    vinod_maven

---
+++ src/main/java/kasak/WelcomeEndpoint.java
@@ -0,0 +1,24 @@
+package kasak;
+
+import static org.eiichiro.bootleg.MediaType.*;
+import static org.eiichiro.bootleg.Verb.*;
+
+import org.eiichiro.jaguar.inject.Inject;
+import org.eiichiro.jaguar.inject.Name;
+import org.eiichiro.bootleg.annotation.Allows;
+import org.eiichiro.bootleg.annotation.Endpoint;
+import org.eiichiro.bootleg.annotation.Generates;
+
+@Name("kasak.WelcomeEndpoint")
+@Endpoint
+public class WelcomeEndpoint {
+
+	@Inject private WelcomeService service;
+	
+	@Allows(GET)
+	@Generates(APPLICATION_JSON)
+	public Welcome get() {
+		return service.get();
+	}
+	
+}
